974aa6227fb0e56025137c688b0a55d7791c3666
Vipul Naik <vipulnaik1@gmail.com>
2019-06-04 10:00:44 -0700

    Start adding tags to some documents

--- sql/documents-schema.sql
+++ sql/documents-schema.sql
@@ -7,6 +7,7 @@ create table documents(
   author varchar(200),
   publisher varchar(40),
   other_participants_or_mentioned_people varchar(2000),
+  tags varchar(2000), /* Pipe-separated list taking values from "Animal consciousness", "Animal harm per unit food consumption", "Personal journey", "Exhortation against animal consumption", "Dealing with moral uncertainty", "Skepticism of case for vegetarianism", "Health effects of veganism", "Practical vegan and vegetarian food advice"
   notes varchar(2000),
   unique key url(`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
