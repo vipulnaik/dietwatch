59c1da883088fd457e0856f12d4c7a1d26c99527
Vipul Naik <vipulnaik1@gmail.com>
2019-06-23 10:22:16 -0700

    Add column displaying people cluster

--- sql/documents-schema.sql
+++ sql/documents-schema.sql
@@ -7,8 +7,9 @@ create table documents(
   author varchar(200),
   publisher varchar(40),
   other_participants_and_mentioned_people varchar(2000),
-  tags varchar(2000), /* Pipe-separated list taking values from "Animal consciousness", "Animal harm per unit food consumption", "Personal journey", "Exhortation against animal consumption", "Dealing with moral uncertainty", "Skepticism of case for vegetarianism", "Health effects of veganism", "Practical vegan and vegetarian food advice" */
+  tags varchar(2000), /* Pipe-separated list taking values from "Animal consciousness", "Animal harm per unit food consumption", "Personal journey", "Exhortation against animal consumption", "Dealing with moral uncertainty", "Skepticism of case for vegetarianism", "Health effects of veganism", "Practical vegan and vegetarian food advice", "Skepticism of advocacy" */
   animal_types varchar(200), /* Pipe-separated list taking values from "Fish", "Insect", "Bivalve", "Poultry", "Cattle", "Pig" */
+  people_clusters varchar(2000), /* Effective altruist, Libertarian, Negative utilitarian, Tomasik, Caplan, Hanson */
   notes varchar(2000),
   unique key url(`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
