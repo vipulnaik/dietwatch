3e9405665dde860693f9818ff9f24cd654866542
Vipul Naik <vipulnaik1@gmail.com>
2022-07-10 17:17:02 -0700

    Add support for anchorjs and change-theme

--- access-portal/index.php
+++ access-portal/index.php
@@ -1,11 +1,13 @@
 <?php
 include_once('doctype.inc');
 include_once('analytics.inc');
+print '<script src="change-theme.js"></script>';
 include_once('strip-commas.inc');
 include_once('backend/stringFunctions.inc');
 include_once('backend/cachingFunctions.inc');
 print "<title>Diet Watch: Documents and People</title>";
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
+include_once('style.inc');
 print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
 print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
 print '</head>';
@@ -17,6 +19,7 @@ print '<script>$(document).ready(function()
     }
 ); </script>'."\n";
 print "<h3>Diet Watch: Documents and People</h3>";
+print '<p><span id="changeThemeMenu" style="display: none;">Change <a href="javascript:;" onclick="change_theme_color()">color</a></span>   &thinsp;<!-- blank space to prevent cumulative layout shift --></p>';
 include_once('preamble.inc');
 print '<h4>Table of contents</h4>';
 print '<ul>';
